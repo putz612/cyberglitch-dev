e4d12c8c1aa39ee84181538d1c10addb8f567c1b
Jason Sievert <jason.sievert@serko.com>
2020-08-20 14:47:09 -0500

    Updates

---
+++ precache-manifest.f2b5c191fc0c21e48c022a34c29f4195.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f24d6c5604f118ae3b9d80b926d3774b",
+    "url": "/cyberglitch-dev/index.html"
+  },
+  {
+    "revision": "0a9fc0934ad5dbbb32b3",
+    "url": "/cyberglitch-dev/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3b3d612b0f0598faf987",
+    "url": "/cyberglitch-dev/static/js/2.896b9cb5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cyberglitch-dev/static/js/2.896b9cb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a9fc0934ad5dbbb32b3",
+    "url": "/cyberglitch-dev/static/js/main.771dd5cd.chunk.js"
+  },
+  {
+    "revision": "51f2c58ba632d69bc98d",
+    "url": "/cyberglitch-dev/static/js/runtime-main.a290db8e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cyberglitch-dev/static/media/logo.5d5d9eef.svg"
+  }
+]);
